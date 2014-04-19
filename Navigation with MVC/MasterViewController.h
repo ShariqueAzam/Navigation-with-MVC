4f80b47d95541ae6973e2c698ef4d7e75a1f567e
ShariqueAzam <student1@CR33S30I.local>
2014-04-20 01:36:37 +0800

    space
    space update

--- Navigation with MVC/MasterViewController.h
+++ Navigation with MVC/MasterViewController.h
@@ -10,7 +10,7 @@
 
 @interface MasterViewController : UITableViewController
 {
-    NSMutableArray *courses;
+      NSMutableArray *courses;
 }
 
 @property (nonatomic, assign)NSMutableArray *courses;
